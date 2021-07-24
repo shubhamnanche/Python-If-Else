108858a08ac4d7553675d1ea5b52947afabb3ea4
shunax19 <shubhamnanche19@gmail.com>
2021-07-24 20:29:45 +0530

    Shubham-First-Commit

--- if_else.py
+++ if_else.py
@@ -1,6 +1,6 @@
 n = int(input("Enter n :- "))
 
 if (n%2 != 0) or (n%2 ==0 and 6<=n<=20):
-    print("Weird 000000000")
+    print("Weird ")
 elif (n%2 == 0 and 2<=n<=5) or (n%2 == 0 and 20<n) :
-    print("Not Weird")
+    print("Not Weird ghhgkjdwhqjhwdqkj")
