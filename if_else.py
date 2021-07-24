3c10cdb856e4e4e82d52b9fb999def5913df807f
aniketaniket <aniketambore0@gmail.com>
2021-07-24 19:52:27 +0530

    If-else bug fixed

--- if_else.py
+++ if_else.py
@@ -1,6 +1,6 @@
 n = int(input("Enter n :- "))
 
 if (n%2 != 0) or (n%2 ==0 and 6<=n<=20):
-    print("Weird")
+    print("Weird 000000000")
 elif (n%2 == 0 and 2<=n<=5) or (n%2 == 0 and 20<n) :
     print("Not Weird")
